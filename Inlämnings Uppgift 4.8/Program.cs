f874ddecc6034a7d314c1831781b5a702fd49205
baronvonpurr <robinhood.svensson@hotmail.com>
2023-10-22 17:39:48 +0200

    Add project files.

---
+++ Inlämnings Uppgift 4.8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Uppgift4_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hittade = "";
+            Console.WriteLine("Skriv en sträng med text");
+            String text = Console.ReadLine().ToLower();
+            Console.WriteLine("Skriv in två tecken");
+            String tecken = Console.ReadLine().ToLower();
+
+            for (int i = 0; i < tecken.Length; i++)
+            {
+                char först = tecken[i];
+                for (int j = 0; j < text.Length; j++)
+                {
+                    char sist = text[j];
+
+                    if (först == sist)
+                    {
+                        hittade = hittade + först;
+                    }
+                }
+            }
+            if (hittade == tecken)
+            {
+                Console.WriteLine("Båda bokstäverna finns");
+            }
+            else
+            {
+                Console.WriteLine("Båda tecken fanns inte");
+            }
+        }
+    }
+}
